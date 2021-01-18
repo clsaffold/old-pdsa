cda55fd736b695422047000ee8ab431d59319901
csaffold <ceciliasaffold@gmail.com>
2021-01-18 15:41:18 -0600

    Set Db Context

---
+++ pdsatracker/Data/PdsaData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using PdsaTracker.Models
+using Microsoft.EntityFrameworkCore;
+
+namespace PdsaTracker.Data
+
+{
+	public class PdsaDbContext: DbContext
+	{
+		public DbSet<Pdsa> Pdsas { get; set; }
+
+		public PdsaDbContext(DbContextOptions<PdsaDbContext> options) : base(option)
+		//static private Dictionary<int, Pdsa> Entries = new Dictionary<int, pdsa>();
+		{
+		}
+	}
+}
