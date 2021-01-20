da3af0c82af6a090515a29c4d0145558b233ae31
csaffold <ceciliasaffold@gmail.com>
2021-01-20 14:57:27 -0600

    Updated DbContext for Identity and App to UseMySql

--- Startup.cs
+++ Startup.cs
@@ -26,10 +26,10 @@ namespace pdsa
         {
             services.AddControllersWithViews();
             services.AddRazorPages();
-            services.AddDbContext<PdsaTracker.Data.PdsaDbContext>(options =>options.UseMySql(Configuration.GetConnectionString("DefaultConnection")));
-        }
+            services.AddDbContext<PdsaTracker.Data.PdsaDbContext>(options => options.UseMySql(Configuration.GetConnectionString("DefaultConnection")));
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        }
+            // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
