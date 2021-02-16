c033edfc674b14b9f878ac78159a3b4e3b66bda2
csaffold <ceciliasaffold@gmail.com>
2021-02-16 16:20:29 -0600

    SendGrid Configured and Tested

--- obj/Debug/net5.0/pdsatracker.AssemblyInfo.cs
+++ obj/Debug/net5.0/pdsatracker.AssemblyInfo.cs
@@ -12,6 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: Microsoft.AspNetCore.Identity.UI.UIFrameworkAttribute("Bootstrap4")]
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("a7dbd9c2-3ae3-418c-93ab-343268f75c12")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("pdsatracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
